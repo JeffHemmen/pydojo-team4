eae88678d6176ae00bcfef4eafb40e58cfa315e0
Davy Kavanagh <davy.kavanagh@intercom.io>
2019-11-07 20:18:19 +0000

    some stuff

--- main.py
+++ main.py
@@ -1,5 +1,27 @@
-HEIGHT = 300
-WIDTH = 300
+alien = Actor('spaceship')
+alien.pos = 100, 56
+
+WIDTH = 500
+HEIGHT = 500
+
+SPEED = 10
 
 def draw():
-    screen.fill((128, 0, 0))
+    screen.clear()
+    alien.draw()
+    
+    
+def on_key_down(key):
+    if key == keys.W:
+        alien.x += 1
+        alien.y += 1
+    elif key == keys.A:
+        alien.angle -= 1
+    elif key == keys.D:
+        alien.angle += 1
+    elif key == keys.S:
+        alien.x -= 1
+        alien.y -= 1
+        
+        
+        
