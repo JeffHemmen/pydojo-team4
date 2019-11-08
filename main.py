2a07ade06f174278662ada90c077516fee461f12
Davy Kavanagh <davy.kavanagh@intercom.io>
2019-11-08 11:33:09 +0000

    rotation and collision detection

--- main.py
+++ main.py
@@ -9,23 +9,36 @@ black_hole.pos = (500,500)
 
 player1 = Actor('spaceship')
 player1.pos = 50, 50
-player1.vel = 0, 50
+player1.vel = 0, 200
 player1.acc = 0, 0
+
+black_hole_rect = Rect(black_hole.pos, (10, 10))
 
 
 def update_player(p):
-    p.acc = ((black_hole.pos[0] - p.pos[0]) // 100 , (black_hole.pos[1] - p.pos[1]) // 100)
+    p.acc = ((black_hole.pos[0] - p.pos[0]) // 20 , (black_hole.pos[1] - p.pos[1]) // 20)
     p.pos = (p.pos[0] + p.vel[0], p.pos[1] + p.vel[1])
     p.vel = ((p.vel[0] + p.acc[0]) // 2, (p.vel[1] + p.acc[1]) // 2)
     return
 
-def my_update():
+def update():
     update_player(player1)
-    pygame.time.delay(10)
+    
+    if keyboard[keys.A]:
+        player1.angle -= 10
+    elif keyboard[keys.D]:
+        player1.angle += 10
+        
+    if player1.colliderect(black_hole_rect):
+        print('ho noes!!!')
+    
+    
+    
+    
 
-clock.schedule_interval(my_update, 1/30)
+# clock.schedule_interval(my_update, 1/30)
 
 def draw():
     screen.fill((128, 128, 0))
-    screen.draw.circle(black_hole.pos, 5, (0,0,0))
+    screen.draw.circle(black_hole.pos, 10, (0,0,0))
     player1.draw()
